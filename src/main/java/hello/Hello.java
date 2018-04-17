f64cbe29657166bc1020cd95a9304c8e3ae8885f
pc <pc@pc-PC>
2018-04-17 12:42:29 +0800

    4.17

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -37,6 +37,9 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Wodeadfsdfdsf");
 		System.out.println("nihao");
+		System.out.println("nihao");
+		System.out.println("nihao");
+		System.out.println("nihao");
 	}
 
 }
