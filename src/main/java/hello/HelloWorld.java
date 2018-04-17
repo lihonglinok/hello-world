5f0846872aab8ea8bee5c2fc7acf2cfd85880ceb
sina <771767817@qq.com>
2018-04-17 12:55:49 +0800

    2018

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,76 @@
+package hello;
+
+import java.io.Serializable;
+
+public class HelloWorld implements Serializable{
+	private static final long serialVersionUID = -5987833013559001844L;
+	private Integer id;//员工ID
+	private String name;//员工姓名
+	private Integer salary;//员工工资
+	public HelloWorld() {
+		super();
+	}
+	public HelloWorld(Integer id, String name, Integer salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getSalary() {
+		return salary;
+	}
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((salary == null) ? 0 : salary.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HelloWorld other = (HelloWorld) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (salary == null) {
+			if (other.salary != null)
+				return false;
+		} else if (!salary.equals(other.salary))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "HelloWorld [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+}
