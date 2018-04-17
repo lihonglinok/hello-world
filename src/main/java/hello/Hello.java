d35e75cde753afb345241a5d3e2e5eb46dab5cbe
pc <pc@pc-PC>
2018-04-17 11:17:38 +0800

    2018

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -37,6 +37,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Wodeadfsdfdsf");
+		System.out.println("Wodeadfsdfdsf");
+		System.out.println("Wodeadfsdfdsf");
 	}
 
 }
